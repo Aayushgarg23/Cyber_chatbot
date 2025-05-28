3433328dc30a3fc41084415365e87f30643900c2
Aayush Garg <142904158+Aayushgarg23@users.noreply.github.com>
2025-05-28 23:33:46 +0530

    Update config.js

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -1,16 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB3YRIoSokGNLrTxMbDRBhYfHHR73o-UV8",
-  authDomain: "quirckart-infotech.firebaseapp.com",
-  databaseURL: "https://quirckart-infotech-default-rtdb.firebaseio.com",
-  projectId: "quirckart-infotech",
-  storageBucket: "quirckart-infotech.firebasestorage.app",
-  messagingSenderId: "1011544239867",
-  appId: "1:1011544239867:web:84e790ac1093508ed60a5e",
-  measurementId: "G-0KTRRVB0M2"
-};
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
