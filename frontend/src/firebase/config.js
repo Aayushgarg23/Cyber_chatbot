43640d00b6464546e397087451d843ae7c26d2df
Aayush Garg <142904158+Aayushgarg23@users.noreply.github.com>
2025-05-28 23:34:22 +0530

    Update config.js

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-
+{/* Add Firebase Auth Details*/}
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
